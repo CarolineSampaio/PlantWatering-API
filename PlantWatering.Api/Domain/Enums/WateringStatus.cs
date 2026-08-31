063650e23d1bd60b0652a1bc102d567877b4d446
Caroline Sampaio <caroline_08022@hotmail.com>
2026-08-30 21:44:55 -0300

    feat: modelo de dominio Plant e enums de controle

--- PlantWatering.Api/Domain/Enums/WateringStatus.cs
+++ PlantWatering.Api/Domain/Enums/WateringStatus.cs
@@ -1,5 +1,11 @@
 namespace PlantWatering.Api.Domain.Enums;
 
+/// <summary>
+/// representa o status atual do ciclo de rega da planta
+/// </summary>
 public enum WateringStatus
 {
+    UpToDate = 1,
+    DueToday = 2,
+    Overdue = 3
 }
