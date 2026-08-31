0d11f86ba66c40be763b40e93a20a7b52a6a8cd3
Caroline Sampaio <caroline_08022@hotmail.com>
2026-08-30 21:59:09 -0300

    feat: contrato e repositorio em memoria IPlantRepository

--- PlantWatering.Api/Data/Interfaces/IPlantRepository.cs
+++ PlantWatering.Api/Data/Interfaces/IPlantRepository.cs
@@ -1,5 +1,16 @@
+using PlantWatering.Api.Domain.Models;
+
 namespace PlantWatering.Api.Data.Interfaces;
 
+/// <summary>
+/// contrato de persistencia para operacoes com a entidade Plant
+/// </summary>
 public interface IPlantRepository
 {
+    Task<IEnumerable<Plant>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<Plant?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Plant>> GetPendingWateringAsync(CancellationToken cancellationToken = default);
+    Task<Plant> AddAsync(Plant plant, CancellationToken cancellationToken = default);
+    Task<bool> UpdateAsync(Plant plant, CancellationToken cancellationToken = default);
+    Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
 }
